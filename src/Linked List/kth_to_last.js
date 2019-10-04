bf5ed9dd877f5497cd13613e85b0c4d8da8dc6f5
ilyasshabi <ilyasshabi94@gmail.com>
2019-10-04 16:39:44 +0200

    Kth to last element Using two pointers

--- src/Linked List/kth_to_last.js
+++ src/Linked List/kth_to_last.js
@@ -7,6 +7,16 @@ export function main(list, k) {
     return list.value;
 }
 
+export function twoPointers(list, k){
+    let follower = list;
+    for(let i = 0; i<k; i++) list = list.next;
+    while(list.next){
+        list = list.next;
+        follower = follower.next;
+    }
+    return follower.value;
+}
+
 function _getLength (list) {
     let length = 0;
     while (list.next) {
--- src/Linked List/kth_to_last.test.js
+++ src/Linked List/kth_to_last.test.js
@@ -1,4 +1,4 @@
-import { main } from './kth_to_last';
+import { main, twoPointers } from './kth_to_last';
 import { LinkedList } from './singlyLinkedList';
 
 describe('Remove duplicate from Linked List', () => {
@@ -9,4 +9,48 @@ describe('Remove duplicate from Linked List', () => {
         
         expect(main(list, 2)).toEqual(3);
     });
+    it('Kth to last element - two pointers', () => {
+        const input = [1, 2, 3, 4, 5];
+        let list = new LinkedList(input.shift());
+        list.arrayToLinkedList(input);
+        
+        expect(twoPointers(list, 2)).toEqual(3);
+    });
+    it('Edge case - two pointers', () => {
+        const input = [8];
+        let list = new LinkedList(input.shift());
+        list.arrayToLinkedList(input);
+        
+        expect(twoPointers(list, 0)).toEqual(8);
+    });
+
+    [
+        {
+          list: [8, 5, 1],
+          k: 0
+        },
+        {
+          list: [8, 5, 1],
+          k: 1
+        },
+        {
+          list: [8, 8, 9, 9, 9, 6, 6, 4, 4, 6, 6, 4, 4, 6, 9, 4, 8, 2, 3, 1],
+          k: 8
+        },
+        {
+          list: [8, 8, 9, 9, 9, 6, 6, 4, 4, 6, 6, 4, 4, 6, 9, 4, 8, 2, 3, 1],
+          k: 16
+        }
+      ].forEach((elem, i) => {
+        it(`${i+1} - Kth to last element - two pointers  - ${elem.list.length - 1 - elem.k} `, () => {
+            const input = elem.list;
+            let list = new LinkedList(input.shift());
+            list.arrayToLinkedList(input);
+            const expected = elem.list[elem.list.length - 1 - elem.k];
+            expect(twoPointers(list, elem.k)).toEqual(expected);
+        });
+      });
 });
+
+
+
