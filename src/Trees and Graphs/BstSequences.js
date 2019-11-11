6d79da87602e623992e5d90805719b5fa29d45fc
ilyasshabi <ilyasshabi94@gmail.com>
2019-11-11 22:08:36 +0100

    Bst sequences

--- src/Trees and Graphs/BstSequences.js
+++ src/Trees and Graphs/BstSequences.js
@@ -1,8 +1,41 @@
 export function getSequences(input) {
-    return getArraysFromTree(input);
+    return getArraysFromTree(input.root);
 }
 
-function getArraysFromTree(input) {
+function getArraysFromTree(tree) {
+    if(tree == null){
+        return;
+    }
+    
+    const leftResult = getArraysFromTree(tree.left);
+    const rightResult = getArraysFromTree(tree.right);
+    if(!leftResult &&  !rightResult){
+        return [tree.val];
+    }
 
-   
+    const result = [];
+    
+    for(let i = 0; i < leftResult.length; ++i){
+        for(let j= 0; j < rightResult.length; ++j){
+            
+            const temp = [tree.val, ...toArray(leftResult[i]), ...toArray(rightResult[j])];
+            result.push(temp);
+        }
+    }
+    for(let i = 0; i < rightResult.length; ++i){
+        for(let j= 0; j < leftResult.length; ++j){
+            const temp = [tree.val, ...toArray(rightResult[i]), ...toArray(leftResult[j])];
+            result.push(temp);
+        }
+    }
+   return result;
+}
+
+function toArray(data){
+
+    if(Array.isArray(data)){
+        return data;
+    }
+    return [data]
+
 }
--- src/Trees and Graphs/BstSequences.test.js
+++ src/Trees and Graphs/BstSequences.test.js
@@ -4,6 +4,34 @@ import { Tree } from './tree';
 describe(`Simple use cases`, () => {
     it(`First test case`, () => {
 
+        //          3
+        //      1       5
+        //   0     2  4    6
+
+        const input = [3, 1, 2, 0, 5, 4, 6];
+        const tree = new Tree();
+
+        input.forEach((elem) => {
+            tree.add(elem);
+        });
+
+        const expected = [
+            [3, 1, 0, 2, 5, 4, 6],
+            [3, 1, 0, 2, 5, 6, 4],
+            [3, 1, 2, 0, 5, 4, 6],
+            [3, 1, 2, 0, 5, 6, 4],
+            [3, 5, 4, 6, 1, 0, 2],
+            [3, 5, 4, 6, 1, 2, 0],
+            [3, 5, 6, 4, 1, 0, 2],
+            [3, 5, 6, 4, 1, 2, 0]
+        ];
+        const result = getSequences(tree);
+
+        expect(result).toEqual(expected);
+
+    });
+    it(`Second test case`, () => {
+
         const input = [2, 1, 3];
         const tree = new Tree();
 
@@ -11,7 +39,10 @@ describe(`Simple use cases`, () => {
             tree.add(elem);
         });
 
-        console.log(tree);
+        const expected = [[2, 1, 3], [2, 3, 1]];
+        const result = getSequences(tree);
+
+        expect(result).toEqual(expected);
 
     });
 });
